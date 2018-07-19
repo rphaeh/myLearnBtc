10d15b86bb9ead3a80809388e647df5c588e6e17
李江 <lijiang@guanfangyiliao.com>
2018-07-19 15:03:54 +0800

    initProgram

---
+++ MyLearnBtc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyLearnBtc
+//
+//  Created by rphaeh on 2018/7/19.
+//  Copyright © 2018年 rphaeh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
